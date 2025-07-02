419912324b066ee8b579f01e98adf0a8fdc3c786
srwesteban <sr.w@hotmail.com>
2025-07-01 19:43:29 -0500

    Fix: remove unused ScrambledText import

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -3,7 +3,7 @@
 import PersonalCard from "@/components/background/PersonalCard";
 import { About } from "../about/About";
 
-export default function AboutSection() {
+export default function Hero() {
   return (
     <section className="w-full sm:h-screen flex justify-center items-center p-4 sm:pt-0">
       <div className="w-full max-w-[1800px] h-full flex flex-col-reverse lg:flex-row  gap-0 sm:gap-6">
