6f38d9bf3cc0842de4880f5ee306dd7f26784fdc
srwesteban <sr.w@hotmail.com>
2025-07-01 19:29:21 -0500

    Fix: remove unused ScrambledText import

--- src/components/background/PersonalCard.tsx
+++ src/components/background/PersonalCard.tsx
@@ -25,24 +25,24 @@ export default function PersonalCard() {
             alt="me"
             width={300}
             height={350}
-            className="rounded-xl shadow-lg w-[140px] sm:w-[180px] md:w-[260px] h-auto transition-all"
+            className="rounded-xl shadow-lg w-[120px] sm:w-[180px] md:w-[260px] h-auto transition-all"
             priority
           />
 
           {/* Botón contenido y "flotante visualmente" */}
           <button
             className="
-              -mt-6
-              bg-[#2c365d6c] backdrop-blur-3xl
-              px-[clamp(0.75rem,2vw,1.5rem)] py-[clamp(0.5rem,1.2vw,0.9rem)]
-              rounded-xl shadow-md
-              flex items-center justify-center gap-[clamp(0.25rem,1vw,0.75rem)]
-              w-[clamp(160px,50vw,260px)]
-              text-center
-              z-10
-            "
+    -mt-2 sm:-mt-6
+    px-2 sm:px-6 md:px-8
+    py-2 sm:py-2.5 md:py-3
+    w-[130px] sm:w-[220px] md:w-[260px]
+    rounded-xl shadow-md
+    flex items-center justify-center gap-2 sm:gap-3
+    bg-[#2c365d6c] backdrop-blur-3xl
+    text-center z-10
+  "
           >
-            <p className="text-[clamp(0.75rem,1vw,1.125rem)] text-green-400">
+            <p className="text-sm sm:text-base md:text-lg text-green-400">
               Online
             </p>
           </button>
--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -5,13 +5,12 @@ import { About } from "../about/About";
 
 export default function AboutSection() {
   return (
-    <section className="w-full h-screen flex justify-center items-center px-4 pt-24 sm:pt-0">
-      <div className="w-full max-w-[1800px] h-full flex flex-col-reverse lg:flex-row gap-6">
+    <section className="w-full sm:h-screen flex justify-center items-center p-4 sm:pt-0">
+      <div className="w-full max-w-[1800px] h-full flex flex-col-reverse lg:flex-row  gap-0 sm:gap-6">
         {/* About primero en desktop */}
         <div className="w-full lg:w-[80%] h-[80%] lg:h-full">
           <About />
         </div>
-
         {/* PersonalCard arriba en móvil */}
         <div className="w-full lg:w-[20%] h-[20%] lg:h-full">
           <PersonalCard />
